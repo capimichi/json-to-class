8a6988b2af00614d10335893f427c2a003a582e5
Michele Capichioni <capimichi@gmail.com>
2023-12-11 23:26:30 +0100

    Created file: src

--- src/Enum/ExportTypeEnum.php
+++ src/Enum/ExportTypeEnum.php
@@ -4,15 +4,17 @@ namespace App\Enum;
 
 class ExportTypeEnum
 {
-    const JSON_SCHEMA_TYPE  = 'json_schema';
-    const PTYHON_MODEL_TYPE = 'python_model';
+    const JSON_SCHEMA_TYPE     = 'json_schema';
+    const PTYHON_MODEL_TYPE    = 'python_model';
+    const PTYHON_PYDANTIC_TYPE = 'python_pydantic';
     
     
     public static function getChoices(): array
     {
         return [
-            'JSON Schema'  => self::JSON_SCHEMA_TYPE,
-            'Python model' => self::PTYHON_MODEL_TYPE,
+            'JSON Schema'     => self::JSON_SCHEMA_TYPE,
+            'Python model'    => self::PTYHON_MODEL_TYPE,
+            'Python Pydantic' => self::PTYHON_PYDANTIC_TYPE,
         ];
     }
 }
