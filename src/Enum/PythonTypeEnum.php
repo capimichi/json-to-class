319263cd6dd44865de6fa0c66684cb1d79b8e365
Michele Capichioni <capimichi@gmail.com>
2023-12-11 23:26:57 +0100

    Created file: src

---
+++ src/Enum/PythonTypeEnum.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace App\Enum;
+
+
+class PythonTypeEnum
+{
+    
+    const TYPE_STRING   = 'str';
+    const TYPE_INTEGER  = 'int';
+    const TYPE_FLOAT    = 'float';
+    const TYPE_BOOLEAN  = 'bool';
+    const TYPE_LIST     = 'List';
+    const TYPE_DICT     = 'Dict';
+    const TYPE_OBJECT   = 'object';
+    const TYPE_ANY      = 'Any';
+    const TYPE_OPTIONAL = 'Optional';
+    
+}
